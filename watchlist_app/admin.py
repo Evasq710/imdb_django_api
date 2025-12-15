36dd45b17adfd5e2ee2fdd8231b47502ef4768a0
Elías Vasquez <78238476+Evasq710@users.noreply.github.com>
2025-12-15 11:02:58 -0600

    fixing comments

--- watchlist_app/admin.py
+++ watchlist_app/admin.py
@@ -4,5 +4,5 @@ from watchlist_app.models import Movie
 
 # Register your models here.
 
-# Registering our model, in order to use make queries using Djang Admin Panel
+# Registering our model, in order to make queries using Djang Admin Panel
 admin.site.register(Movie)
--- watchlist_app/views.py
+++ watchlist_app/views.py
@@ -6,22 +6,30 @@ from django.http import JsonResponse
 
 # GET ALL MOVIES
 def movie_list(request):
+  # COMPLEX DATATYPE
   movies_qs = Movie.objects.all() # returns a QuerySet Object in the form of a list of registers
   movies = movies_qs.values() # returns a QuerySet Object in the form of a list of python dictionaries
+
   data = {
+    # SERIALIZATION
     'movies': list(movies) # list() will transform QuerySet Object into a list of valid python dictionaries
   }
 
+  # RENDER INTO JSON
   return JsonResponse(data)
 
 
 # GET AN SPECIFIC MOVIE
 def movie_details(request, pk):
+  # COMPLEX DATATYPE
   movie = Movie.objects.get(pk=pk) # returns a Movie Object if found
+
+  # SERIALIZATION
   data = {
     'name': movie.name,
     'description': movie.description,
     'active': movie.active,
   }
 
+  # RENDER INTO JSON
   return JsonResponse(data)
