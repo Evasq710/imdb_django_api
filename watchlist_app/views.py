e355ad6fa46b491265ce8731de84eaf7e7d9684a
Elías Vasquez <78238476+Evasq710@users.noreply.github.com>
2025-12-15 10:33:02 -0600

    GET ALL MOVIES endpoint

--- watchlist_app/views.py
+++ watchlist_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from watchlist_app.models import Movie
+from django.http import JsonResponse
 
 # Create your views here.
+
+# GET ALL MOVIES
+def movie_list(request):
+  movies_qs = Movie.objects.all() # returns a QuerySet Object in the form of a list of registers
+  movies = movies_qs.values() # returns a QuerySet Object in the form of a list of python dictionaries
+  data = {
+    'movies': list(movies) # list() will transform QuerySet Object into a list of valid python dictionaries
+  }
+
+  return JsonResponse(data)
