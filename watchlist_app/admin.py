8d59cfb2f77cf56951b014961804b63c80d41daa
Elías Vasquez <78238476+Evasq710@users.noreply.github.com>
2025-12-15 10:14:36 -0600

    'Movie' model + Registration in Admin panel

--- watchlist_app/admin.py
+++ watchlist_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+# APP MODELS
+from watchlist_app.models import Movie
 
 # Register your models here.
+
+# Registering our model, in order to use make queries using Djang Admin Panel
+admin.site.register(Movie)
